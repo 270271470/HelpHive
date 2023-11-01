73d023d266921e210b5e00e0ef56f39223dfdc23
270271470 <270271470@yoobeestudent.ac.za>
2023-11-01 14:06:22 +1300

    Add project files.

---
+++ Services/DatabaseConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HelpHive.Services
+{
+    public class DatabaseConnection
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConnection(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public MySqlConnection CreateConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (MySqlConnection connection = CreateConnection())
+                {
+                    connection.Open(); // This will throw an exception if there's an issue connecting.
+                    connection.Close();
+                    return true; // Returns true if connection is successful.
+                }
+            }
+            catch
+            {
+                return false; // Returns false if any exception occurs.
+            }
+        }
+
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HelpHive.Services;
+
+namespace HelpHive.Views
+{
+    /// Interaction logic for MainWindow.xaml
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = "Server=223.165.67.19;Database=helphive_db;User ID=helphive_admin;Password=qD7On3mDrwAr;SslMode=Required;";
+
+            var dbConnection = new DatabaseConnection(connectionString);
+
+            bool isConnected = dbConnection.TestConnection();
+
+            if (isConnected)
+            {
+                MessageBox.Show("Connected to the database successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Failed to connect to the database.");
+            }
+        }
+    }
+}
